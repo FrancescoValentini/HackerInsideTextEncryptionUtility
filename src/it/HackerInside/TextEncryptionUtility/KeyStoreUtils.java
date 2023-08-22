97042585563dba985d4203a6d5f2be0463f292ed
Francesco Valentini <Francesco.valentini930@gmail.com>
2023-08-23 00:19:27 +0200

    Aggiunta libreria Bouncy Castle e adozione del formato BCFKS per il keystore

--- src/it/HackerInside/TextEncryptionUtility/KeyStoreUtils.java
+++ src/it/HackerInside/TextEncryptionUtility/KeyStoreUtils.java
@@ -8,43 +8,59 @@ import java.security.Key;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.Security;
 import java.security.UnrecoverableKeyException;
 import java.security.cert.CertificateException;
 import java.util.Enumeration;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 import javax.crypto.SecretKey;
+import javax.swing.JOptionPane;
 
 public class KeyStoreUtils {
-	   public static void saveKeyStore(KeyStore ks, String masterPassword,String filename) throws KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException {
-		   try (FileOutputStream fos = new FileOutputStream(filename)) {
-			    ks.store(fos, masterPassword.toCharArray());
-			    fos.close();
-			}
-	   }
-	   
-	   public static KeyStore loadKeyStore(String masterPassword,String filename) throws NoSuchAlgorithmException, CertificateException, FileNotFoundException, IOException, KeyStoreException {
-		   KeyStore ks  = KeyStore.getInstance("JCEKS");
-		   ks.load(new FileInputStream(filename), masterPassword.toCharArray());
-		   return ks;
-	   }
-	   
-	   public static void addSecretKey(KeyStore ks, String masterPassword, String alias, SecretKey key) throws KeyStoreException {
-		  KeyStore.SecretKeyEntry secret = new KeyStore.SecretKeyEntry(key);
-		  KeyStore.ProtectionParameter password= new KeyStore.PasswordProtection(masterPassword.toCharArray());
-		  ks.setEntry(alias, secret, password);
-	   }
-	   public static SecretKey getSecretKey(KeyStore ks, String masterPassword, String alias) throws KeyStoreException, UnrecoverableKeyException, NoSuchAlgorithmException {
-		  return (SecretKey) ks.getKey(alias, masterPassword.toCharArray());
-	   }
-	   
-	   public static Key getKey(KeyStore ks, String masterPassword, String alias) throws KeyStoreException, UnrecoverableKeyException, NoSuchAlgorithmException {
-		  return ks.getKey(alias, masterPassword.toCharArray());
-	   }
-	   
-	   public static void deleteSecretKey(KeyStore ks, String masterPassword, String alias) throws KeyStoreException {
-		   ks.deleteEntry(alias);
-	   }
-	   public static Enumeration<String> showAliases(KeyStore ks) throws KeyStoreException{
-		   return ks.aliases();
-	   }
+	
+	static {
+		Security.addProvider(new BouncyCastleProvider());
+	}
+	
+	public static KeyStore newKeystore(String masterPassword, String filename) throws KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException, NoSuchProviderException { // Configurazione iniziale di un nuovo keystore
+		KeyStore ks = KeyStore.getInstance("BCFKS", "BC");
+		ks.load(null, masterPassword.toCharArray());
+		KeyStoreUtils.saveKeyStore(ks,masterPassword,filename);
+		return ks;
+	}
+   public static void saveKeyStore(KeyStore ks, String masterPassword, String filename) throws KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException {
+       try (FileOutputStream fos = new FileOutputStream(filename)) {
+           ks.store(fos, masterPassword.toCharArray());
+       }
+   }
+
+   public static KeyStore loadKeyStore(String masterPassword, String filename) throws NoSuchAlgorithmException, CertificateException, FileNotFoundException, IOException, KeyStoreException, NoSuchProviderException {
+       KeyStore ks = KeyStore.getInstance("BCFKS", "BC");
+       ks.load(new FileInputStream(filename), masterPassword.toCharArray());
+       return ks;
+   }
+
+   public static void addSecretKey(KeyStore ks, String masterPassword, String alias, SecretKey key) throws KeyStoreException {
+      KeyStore.SecretKeyEntry secret = new KeyStore.SecretKeyEntry(key);
+      KeyStore.ProtectionParameter password = new KeyStore.PasswordProtection(masterPassword.toCharArray());
+      ks.setEntry(alias, secret, password);
+   }
+
+   public static SecretKey getSecretKey(KeyStore ks, String masterPassword, String alias) throws KeyStoreException, UnrecoverableKeyException, NoSuchAlgorithmException {
+      return (SecretKey) ks.getKey(alias, masterPassword.toCharArray());
+   }
+
+   public static Key getKey(KeyStore ks, String masterPassword, String alias) throws KeyStoreException, UnrecoverableKeyException, NoSuchAlgorithmException {
+      return ks.getKey(alias, masterPassword.toCharArray());
+   }
+
+   public static void deleteSecretKey(KeyStore ks, String masterPassword, String alias) throws KeyStoreException {
+       ks.deleteEntry(alias);
+   }
+
+   public static Enumeration<String> showAliases(KeyStore ks) throws KeyStoreException{
+       return ks.aliases();
+   }
 }
